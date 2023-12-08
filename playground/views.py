d4cca967f5733c8b313d4da44d1df509175c17df
Arun Shekar <shekar.arun@gmail.com>
2023-12-08 14:51:26 +0530

    meta: Add debugger configuration and sample function

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def dummy():
+    x = 1
+    y = 2
+    return x + y
+
+
 def say_hello(request):
     # return HttpResponse('Hello World!')
-    return render(request, 'hello.html', {'name': 'Arun'})
+    res = dummy()
+    return render(request, 'hello.html', {'name': 'Arun', 'res': res})
