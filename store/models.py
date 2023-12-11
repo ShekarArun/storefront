06f316a1fbe9d1e08c30beccbc881e9cea82a18c
Arun Shekar <shekar.arun@gmail.com>
2023-12-11 13:39:11 +0530

    model: Add Zip Code to Address Model

--- store/models.py
+++ store/models.py
@@ -73,6 +73,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=10, default='')
     customer = models.OneToOneField(
         Customer, on_delete=models.CASCADE, primary_key=True)
 
