c565e2949606f223838426b555a35eb40b925c40
Arun Shekar <shekar.arun@gmail.com>
2023-12-12 15:20:18 +0530

    db: Change DB to MySQL

--- storefront/settings.py
+++ storefront/settings.py
@@ -83,8 +83,10 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'PASSWORD': 'gw864%2AgQx5ky',  # Only for development
     }
 }
 
