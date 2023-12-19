40f7073d995f4abee968cc21c068a6c20fc5c331
Arun Shekar <shekar.arun@gmail.com>
2023-12-19 17:04:02 +0530

    admin: Add Customer admin page with customization

--- store/admin.py
+++ store/admin.py
@@ -10,5 +10,13 @@ class ProductAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    ordering = ['first_name', 'last_name']
+    list_editable = ['membership']
+    list_per_page = 10
+
+
 admin.site.register(models.Collection)
 # admin.site.register(models.Product)
