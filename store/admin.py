914ba13852875a9b767bf4757b7d6715b5117461
Arun Shekar <shekar.arun@gmail.com>
2023-12-20 20:53:45 +0530

    admin: Add tag entry to Product admin

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,6 @@
 from typing import Any
 from django.contrib import admin, messages
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
 from django.db.models import Count
@@ -7,6 +8,7 @@ from django.utils.html import format_html, urlencode
 from django.urls import reverse
 
 from . import models
+from tags.models import TaggedItem
 
 
 class InventoryFilter(admin.SimpleListFilter):
@@ -23,10 +25,16 @@ class InventoryFilter(admin.SimpleListFilter):
             return queryset.filter(inventory__lt=10)
 
 
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    model = TaggedItem
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     actions = ['clear_inventory']
     autocomplete_fields = ['collection']
+    inlines = [TagInline]
     list_display = ['title', 'unit_price',
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
