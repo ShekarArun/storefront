00d15edbe277a1664b8ae95bc4eac210e2e3658f
Arun Shekar <shekar.arun@gmail.com>
2024-01-03 16:51:11 +0530

    api: Add ModelSerializer to Collection serializer

--- store/serializers.py
+++ store/serializers.py
@@ -3,9 +3,12 @@ from rest_framework import serializers
 from .models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
 
 
 class ProductSerializer(serializers.ModelSerializer):
