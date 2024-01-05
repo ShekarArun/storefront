70fe03124cb81d823c74f41b5b0a73061d85e155
Arun Shekar <shekar.arun@gmail.com>
2024-01-05 22:36:36 +0530

    fix: Fix delete operation for Product and Collection APIs

--- store/views.py
+++ store/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-from .models import Product, Collection
+from .models import Product, Collection, OrderItem
 from .serializers import ProductSerializer, CollectionSerializer
 
 
@@ -18,14 +18,21 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'request': self.request}
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response(
                 {'error': 'Product cannot be deleted as it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED
             )
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+
+    # def delete(self, request, pk):
+    #     product = get_object_or_404(Product, pk=pk)
+    #     if product.orderitems.count() > 0:
+    #         return Response(
+    #             {'error': 'Product cannot be deleted as it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED
+    #         )
+    #     product.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CollectionViewSet(ModelViewSet):
@@ -33,16 +40,23 @@ class CollectionViewSet(ModelViewSet):
         products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(
-            Collection.objects.annotate(products_count=Count('products')), pk=pk
-        )
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response(
                 {'error': 'Collection cannot be deleted as it has one or more products associated with it.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED
             )
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
+
+    # def delete(self, request, pk):
+    #     collection = get_object_or_404(
+    #         Collection.objects.annotate(products_count=Count('products')), pk=pk
+    #     )
+    #     if collection.products.count() > 0:
+    #         return Response(
+    #             {'error': 'Collection cannot be deleted as it has one or more products associated with it.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED
+    #         )
+    #     collection.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # Removed since all features are a part of the Product viewset
