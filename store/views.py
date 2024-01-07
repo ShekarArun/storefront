55157c262056f957b82583124bbf580c06538298
Arun Shekar <shekar.arun@gmail.com>
2024-01-07 15:43:24 +0530

    feat: Add search and sort options to product list API

--- store/views.py
+++ store/views.py
@@ -1,11 +1,12 @@
 from django.db.models import Count
-from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
+# from django.shortcuts import get_object_or_404
+# from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# from rest_framework.decorators import api_view
+from rest_framework.filters import SearchFilter, OrderingFilter
+# from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 
@@ -17,9 +18,11 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     # filterset_fields = ['collection_id']
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     # def get_queryset(self):
     #     queryset = Product.objects.all()
