bcaf4be1cc8cf25b2b811cea0a9c4d8a769857af
Arun Shekar <shekar.arun@gmail.com>
2024-01-07 15:05:56 +0530

    review: Add nested routers to fetch reviews for products

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
+# from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 from . import views
 
-router = SimpleRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
-urlpatterns = router.urls
+
+products_router = routers.NestedDefaultRouter(
+    router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet,
+                         basename='product-reviews')
+
+urlpatterns = router.urls + products_router.urls
 
 # urlpatterns = [
 #     # path('products/', views.product_list),
