ae6308e5696f9fb89beec8e925dd7762c6036176
Arun Shekar <shekar.arun@gmail.com>
2024-01-09 11:27:22 +0530

    admin: Add user profile for Customer

--- store/models.py
+++ store/models.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.core.validators import MinValueValidator
+from django.contrib import admin
 from django.db import models
 
 from uuid import uuid4
@@ -57,24 +59,34 @@ class Customer(models.Model):
         (MEMBERSHIP_KEY_GOLD, 'Gold'),
     ]
 
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
+    # first_name = models.CharField(max_length=30)
+    # last_name = models.CharField(max_length=30)
+    # email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_KEY_BRONZE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
 
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ['user__first_name', 'user__last_name']
         # Example on how to override default table name
         # db_table = 'store_customers'
-        indexes = [
-            models.Index(fields=['last_name', 'first_name'])
-        ]
+        # indexes = [
+        #     models.Index(fields=['user__last_name', 'user__first_name'])
+        # ]
 
 
 class Order(models.Model):
