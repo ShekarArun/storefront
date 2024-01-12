e26ddfca7ecee2c8029732d28facf37b13e6f58d
Arun Shekar <shekar.arun@gmail.com>
2024-01-12 13:18:35 +0530

    feat: Add endpoint to create orders against customers

--- store/serializers.py
+++ store/serializers.py
@@ -144,5 +144,6 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        print(self.validated_data['cart_id'])
-        print(self.context['user_id'])
+        (customer, created) = Customer.objects.get_or_create(
+            user_id=self.context['user_id'])
+        Order.objects.create(customer=customer)
