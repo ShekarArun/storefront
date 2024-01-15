62a29fe9e7db14ddd106c3565402b006171d028e
Arun Shekar <shekar.arun@gmail.com>
2024-01-15 16:45:31 +0530

    meta: Add atomic transaction logic to create order operation

--- store/serializers.py
+++ store/serializers.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
+from django.db import transaction
 from rest_framework import serializers
+
 from .models import CartItem, Customer, Order, OrderItem, Product, Collection, Review, Cart
 
 
@@ -144,28 +146,29 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        cart_id = self.validated_data['cart_id']
+        with transaction.atomic():
+            cart_id = self.validated_data['cart_id']
 
-        # Custom logic required, where we create both an order and add the relevant order items
-        (customer, created) = Customer.objects.get_or_create(
-            user_id=self.context['user_id'])
-        order = Order.objects.create(customer=customer)
+            # Custom logic required, where we create both an order and add the relevant order items
+            (customer, created) = Customer.objects.get_or_create(
+                user_id=self.context['user_id'])
+            order = Order.objects.create(customer=customer)
 
-        # Extract items currently in cart
-        cart_items = CartItem.objects \
-            .select_related('product') \
-            .filter(cart_id=cart_id)
-        # Generate equivalent order item list
-        order_items = [
-            OrderItem(
-                order=order,
-                product=item.product,
-                unit_price=item.product.unit_price,
-                quantity=item.quantity
-            )
-            for item in cart_items
-        ]
-        # Store order items against the earlier created order
-        OrderItem.objects.bulk_create(order_items)
-        # Finally, remove these items from cart
-        Cart.objects.filter(pk=cart_id).delete()
+            # Extract items currently in cart
+            cart_items = CartItem.objects \
+                .select_related('product') \
+                .filter(cart_id=cart_id)
+            # Generate equivalent order item list
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    unit_price=item.product.unit_price,
+                    quantity=item.quantity
+                )
+                for item in cart_items
+            ]
+            # Store order items against the earlier created order
+            OrderItem.objects.bulk_create(order_items)
+            # Finally, remove these items from cart
+            Cart.objects.filter(pk=cart_id).delete()
